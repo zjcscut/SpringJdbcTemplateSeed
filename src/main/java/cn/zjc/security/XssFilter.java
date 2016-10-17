5e3d0d06129576cc11aab63d1c8d8145a64c60ef
zjcscut <15118850470@163.com>
2016-10-17 23:57:14 +0800

    zjc:添加Xss过滤方案

---
+++ src/main/java/cn/zjc/security/XssFilter.java
@@ -0,0 +1,68 @@
+package cn.zjc.security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.util.WebUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author zjc
+ * @version 2016/10/17 23:16
+ * @description Xss过滤器,可处理multipart请求
+ */
+public class XssFilter extends OncePerRequestFilter {
+
+	private MultipartResolver multipartResolver;
+	private String encoding;
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public XssFilter() {
+	}
+
+	@Override
+	protected void initFilterBean() throws ServletException {
+		XssCommonsMultipartResolver resolver = new XssCommonsMultipartResolver();
+		resolver.setDefaultEncoding(StandardCharsets.UTF_8.name());
+		resolver.setMaxUploadSize(4096000);
+		resolver.setServletContext(getServletContext());
+		multipartResolver = resolver;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+									FilterChain filterChain) throws ServletException, IOException {
+		if (!isBlank(encoding)) {
+			httpServletRequest.setCharacterEncoding(encoding);
+			httpServletResponse.setCharacterEncoding(encoding);
+		}
+		boolean multipartRequestParsed = false;
+		HttpServletRequest request = new XssHttpServletRequestWrapper(httpServletRequest);
+		if (multipartResolver.isMultipart(request)) {
+			request = multipartResolver.resolveMultipart(request);
+			multipartRequestParsed = true;
+		}
+		try {
+			filterChain.doFilter(request, httpServletResponse);
+		} finally {
+			if (multipartRequestParsed) {
+				multipartResolver.cleanupMultipart(WebUtils.getNativeRequest(
+						httpServletRequest, MultipartHttpServletRequest.class
+				));
+			}
+		}
+	}
+
+	private boolean isBlank(String str) {
+		return str == null || str.trim().length() == 0;
+	}
+}
