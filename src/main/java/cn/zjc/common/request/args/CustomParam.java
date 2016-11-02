9c1a5cba549d39c128162564af021de1d16ff384
zjcscut <15118850470@163.com>
2016-11-03 00:32:03 +0800

    zjc:添加quartz-druid配置

--- src/main/java/cn/zjc/common/request/args/CustomParam.java
+++ src/main/java/cn/zjc/common/request/args/CustomParam.java
@@ -7,6 +7,8 @@ import java.util.Date;
  * @author zhangjinci
  * @version 2016/10/31 12:10
  * @function 用于接收特殊表单参数的参数注解
+ * match()的Class数组必须加入相应的类型转换器，详情见{@link cn.zjc.common.converter.StringToDateConverter}
+ * 添加类型转换器见{@link cn.zjc.config.WebMvcConfiguration}
  */
 @Target({ElementType.PARAMETER,ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
--- src/main/java/cn/zjc/utils/DateUtils.java
+++ src/main/java/cn/zjc/utils/DateUtils.java
@@ -1,6 +1,8 @@
 package cn.zjc.utils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,39 +16,42 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class DateUtils {
 
-    public static final String LONG_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
-    public static final String DATE_PATTERN = "yyyy-MM-dd";
-    public static final String TIME_PATTERN = "HH:mm:ss";
-    public static final String LOCAL_LONG_DATETIME_PATTERN = "yyyy年MM月dd日 HH时mm分ss秒";
-    public static final String LOCAL_LONG_YEARMONTH_PATTERN = "yyyy年MM月";
-    public static final String LOCAL_DATE_PATTERN = "yyyy年MM月dd日";
-    public static final String LOCAL_TIME_PATTERN = "HH时mm分ss秒";
+	private static final Logger log = LoggerFactory.getLogger(DateUtils.class);
 
-    //key为pattern,value为SimpleDateFormat实例
-    private static final ConcurrentHashMap<String, SimpleDateFormat> dateFormatCachePool = new ConcurrentHashMap<>();
+	public static final String LONG_DATETIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
+	public static final String DATE_PATTERN = "yyyy-MM-dd";
+	public static final String TIME_PATTERN = "HH:mm:ss";
+	public static final String LOCAL_LONG_DATETIME_PATTERN = "yyyy年MM月dd日 HH时mm分ss秒";
+	public static final String LOCAL_LONG_YEARMONTH_PATTERN = "yyyy年MM月";
+	public static final String LOCAL_DATE_PATTERN = "yyyy年MM月dd日";
+	public static final String LOCAL_TIME_PATTERN = "HH时mm分ss秒";
 
-    public static SimpleDateFormat getInstance(String pattern) {
-        SimpleDateFormat dateFormat = dateFormatCachePool.get(pattern);
-        if (dateFormat == null) {
-            dateFormat = new SimpleDateFormat(pattern);
-            dateFormatCachePool.put(pattern, dateFormat);
-        }
-        return dateFormat;
-    }
+	//key为pattern,value为SimpleDateFormat实例
+	private static final ConcurrentHashMap<String, SimpleDateFormat> dateFormatCachePool = new ConcurrentHashMap<>();
 
-    public static Date parse(String pattern, String source) {
-        Assert.notBlank(source);
-        if (StringUtils.isBlank(pattern)){
-            pattern = LONG_DATETIME_PATTERN;
-        }
-        Date date = null;
-        try {
-            date = getInstance(pattern).parse(source);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        return date;
-    }
+	public static SimpleDateFormat getInstance(String pattern) {
+		SimpleDateFormat dateFormat = dateFormatCachePool.get(pattern);
+		if (dateFormat == null) {
+			dateFormat = new SimpleDateFormat(pattern);
+			dateFormatCachePool.put(pattern, dateFormat);
+		}
+		return dateFormat;
+	}
+
+	public static Date parse(String pattern, String source) {
+		Assert.notBlank(source);
+		Date date = null;
+		try {
+			if (StringUtils.isBlank(pattern)) {
+				date = getInstance(LONG_DATETIME_PATTERN).parse(source);
+			} else {
+				date = getInstance(pattern).parse(source);
+			}
+		} catch (ParseException e) {
+			log.error(String.format("parse %s to java.util.Date with pattern <%s> failed,message:%s", source, pattern, e.getMessage()));
+		}
+		return date;
+	}
 
 
 }
